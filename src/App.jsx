46efbdbafa1776d672fd6c9d0a446794c833829c
kohhorov09 <hubg79894@gmail.com>
2025-06-30 22:39:11 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import VideoChat from "./components/Chat/VideoChat";
 function App() {
   return (
     <div className="App">
+      {/* <Navbar /> */}
       <Routes>
         <Route element={<Navbar />} path="/" />
         <Route element={<VideoChat />} path="videochat" />
